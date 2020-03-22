f86f705602945e859c88c1e427ec273726a981b3
Brendan Gallagher <bpgallagher27@gmail.com>
2020-03-22 13:29:42 -0400

    initial function setup

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 var mysql = require("mysql");
 
+// Creating MySQL connection
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "redsox27",
   database: "employeeTracker_DB"
 });
@@ -17,7 +12,22 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  let initialTable = "SELECT employee.id, employee.first_name, employee.last_name, department.department_name, role.title, role.salary FROM employee LEFT OUTER JOIN department ON employee.id = department.id LEFT OUTER JOIN role ON employee.id = role.id";
-  connection.query(initialTable, function(err, res) {
-    console.table(res);
-})});
+  console.log("Welcome to Employee Manager!")
+});
+
+
+// Creating functions for each action option
+function showEmployees() {
+    let employeeTable = "SELECT employee.id, employee.first_name, employee.last_name, department.department_name, role.title, role.salary FROM employee LEFT OUTER JOIN department ON employee.id = department.id LEFT OUTER JOIN role ON employee.id = role.id";
+    connection.query(employeeTable, function(err, res) {
+        console.table(res);
+    });
+};
+
+function addEmployees() {
+    //
+};
+
+function updateEmployees() {
+    //
+};
