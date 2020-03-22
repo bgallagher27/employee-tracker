571526b878989a6524eb219a00e7a128408bc922
Brendan Gallagher <bpgallagher27@gmail.com>
2020-03-22 13:50:10 -0400

    code formatting

--- server.js
+++ server.js
@@ -24,26 +24,38 @@ function start() {
         type: "list",
         message: "What would you like to do?",
         choices: [
+            "Add an employee",
             "Add a department",
             "Add a role",
-            "Add an employee",
-            "View employees, departments, & roles",
+            "View all employees",
+            "View all departments",
+            "View all roles",
+            "View all employees, departments, & roles",
             "Update employee roles"
         ]
       })
       .then(function(answer) {
         // Call the respective functionality based on user answer
-        if (answer.initialAction === "Add a department") {
+        if(answer.initialAction === "Add an employee") {
+            addEmployee();
+        }
+        else if(answer.initialAction === "Add a department") {
             addDepartment();
         }
         else if(answer.initialAction === "Add a role") {
             addRole();
         }
-        else if(answer.initialAction === "Add an employee") {
-            addEmployee();
-        }
-        else if(answer.initialAction === "View employees, departments, & roles") {
+        else if(answer.initialAction === "View all employees") {
             showEmployees();
+        }
+        else if(answer.initialAction === "View all departments") {
+            showDepartments();
+        }
+        else if(answer.initialAction === "View all roles") {
+            showRoles();
+        }
+        else if(answer.initialAction === "View all employees, departments, & roles") {
+            showAll();
         }
         else if(answer.initialAction === "Update employee roles") {
             updateEmployees();
@@ -56,11 +68,8 @@ function start() {
   
 
 // Creating functions for each action option
-function showEmployees() {
-    let employeeTable = "SELECT employee.id, employee.first_name, employee.last_name, department.department_name, role.title, role.salary FROM employee LEFT OUTER JOIN department ON employee.id = department.id LEFT OUTER JOIN role ON employee.id = role.id";
-    connection.query(employeeTable, function(err, res) {
-        console.table(res);
-    });
+function addEmployee() {
+    //
 };
 
 function addDepartment() {
@@ -71,8 +80,24 @@ function addRole() {
     //
 };
 
-function addEmployee() {
+
+function showEmployees() {
     //
+};
+
+function showDepartments() {
+    //
+};
+
+function showRoles() {
+    //
+};
+
+function showAll() {
+    let employeeTable = "SELECT employee.id, employee.first_name, employee.last_name, department.department_name, role.title, role.salary FROM employee LEFT OUTER JOIN department ON employee.id = department.id LEFT OUTER JOIN role ON employee.id = role.id";
+    connection.query(employeeTable, function(err, res) {
+        console.table(res);
+    });
 };
 
 function updateEmployees() {
