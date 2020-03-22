ba1024c7ee5af86d7e1925bc0329b68faa76acd6
Brendan Gallagher <bpgallagher27@gmail.com>
2020-03-22 13:41:37 -0400

    initial inquirer prompt

--- server.js
+++ server.js
@@ -13,8 +13,47 @@ connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
   console.log("Welcome to Employee Manager!")
+  start();
 });
 
+// function which prompts the user for what action they should take
+function start() {
+    inquirer
+      .prompt({
+        name: "initialAction",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "View employees, departments, & roles",
+            "Update employee roles"
+        ]
+      })
+      .then(function(answer) {
+        // Call the respective functionality based on user answer
+        if (answer.initialAction === "Add a department") {
+            addDepartment();
+        }
+        else if(answer.initialAction === "Add a role") {
+            addRole();
+        }
+        else if(answer.initialAction === "Add an employee") {
+            addEmployee();
+        }
+        else if(answer.initialAction === "View employees, departments, & roles") {
+            showEmployees();
+        }
+        else if(answer.initialAction === "Update employee roles") {
+            updateEmployees();
+        }
+        else{
+          connection.end();
+        }
+      });
+  }
+  
 
 // Creating functions for each action option
 function showEmployees() {
@@ -24,7 +63,15 @@ function showEmployees() {
     });
 };
 
-function addEmployees() {
+function addDepartment() {
+    //
+};
+
+function addRole() {
+    //
+};
+
+function addEmployee() {
     //
 };
 
