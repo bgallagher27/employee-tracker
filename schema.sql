35093ffcf0c45f386f7f565be11d0db7d33e1eef
Brendan Gallagher <bpgallagher27@gmail.com>
2020-03-22 16:00:39 -0400

    fixed salary parameter

--- schema.sql
+++ schema.sql
@@ -21,7 +21,7 @@ CREATE TABLE department(
 CREATE TABLE role(
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL(7,2),
+  salary DECIMAL(9,2),
   department_id INT,
   PRIMARY KEY (id)
 );
